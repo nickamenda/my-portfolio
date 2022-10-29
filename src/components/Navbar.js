6583b55a0e20120353421fc57d35e86940f3187a
Nicholas Amenda <Nickamenda@gmail.com>
2022-10-29 12:10:01 -0500

    create mobile scaling and add github links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,22 +7,28 @@ const Navbar = () => {
     } else {
       x.className = "navbar";
     }
+    var y = document.getElementById("myLinks");
+    if (y.className === "links") {
+      y.className += " responsive";
+    } else {
+      y.className = "links";
+    }
   };
 
   return (
     <>
       <div className="navbar" id="myNavbar">
         <img className="logo" src={logo}></img>
-        <div className="links">
+        <div className="links" id="myLinks">
           <a href="#aboutMe"><section>01.</section>About</a>
           <a href="#projects"><section>02.</section>Projects</a>
           <a href="#work"><section>03.</section>Work</a>
           <a href="#contactMe"><section>04.</section>Contact Me!</a>
           <button href="#">Resume</button>
         </div>
-          {/* <a href="javascript:void(0);" className="icon" onClick={myFunction}>
+          <a href="javascript:void(0);" className="icon" onClick={myFunction}>
             <icon className="fa fa-bars">&#9776;</icon>
-          </a> */}
+          </a>
       </div>
     </>
   )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import sorting from '../Sorting.png'
 import fec from '../Fec.png'
+import Github from '../Github.png'
 
 const Projects = () => {
   return (
@@ -8,31 +9,19 @@ const Projects = () => {
       <div className="projects-intro"><p>While attending the Hack Reactor bootcamp, I worked on many different projects whether it be solo, in pairs or in larger groups of 3-4. I have also worked on quite a few personal projects. Below are some of the projects I have worked on, I would love for you to check them out!</p></div>
       <li className="projects-container">
         <ul>
-          <section><a href="https://nicks-sortingalgo.herokuapp.com/" target="_blank">Sorting Algorithm Visualizer</a></section>
+          <section><a href="https://nicks-sortingalgo.herokuapp.com/" target="_blank">Sorting Algorithm Visualizer</a>
+              <img className="github" src={Github}></img>
+          </section>
           <div className="sorting">
             <p>This is a personal project I worked on with one of my good friends. This visualizer creates a visual depiction of 4 different sorting algorithms. We used a mix of HTML, CSS, and JS to build this visualizer. There are options to change the speed of the algorithm, input how many bars you want shown, and what algorithm you would like to see. Please, feel free to give it a try!</p>
             <img src={sorting} className="sortingSS"></img>
           </div>
         </ul>
         <ul>
-          <section><a href="https://nicks-fec-deploy-test.herokuapp.com/" target="_blank">Frontend Capstone</a></section>
+          <section><a href="https://nicks-fec-deploy-test.herokuapp.com/" target="_blank">Frontend Capstone</a><img src={Github}></img></section>
           <div className="fec">
             <p>This is a 2-week project I also worked on while attending Hack Reactor's bootcamp. We were provided a wireframe of the site design and a business requirements document and were tasked with building the entire app from scratch and deploying it to AWS. I worked in a team of 3 and I took on the role of building out the product overview module which is the first thing you see when going to the site.</p>
             <img src={fec} className="fecSS"></img>
-          </div>
-        </ul>
-        <ul>
-          <section><a href="#">Recastly</a></section>
-          <div className="recastly">
-            <p>This was a 2 day sprint I worked on during my time at Hack Reactor's immersive bootcamp. We were provided the CSS and HTML for this app but had to interact with Youtube's API to get the videos to show, play, and be able to search through Youtube videos. Go ahead and try it out!</p>
-            <img src="#" className="recastlySS"></img>
-          </div>
-        </ul>
-        <ul>
-          <section><a href="#">CS:GO Stat Tracker</a></section>
-          <div className="csgo">
-            <p>This is a personal project that I worked on when I had some free time during my time at Hack Reactor. This was more of a passion project to practice interacting with API's, using React, and styling with CSS. You can input a user's Steam64ID and as long as they have a public profile, it will get all the information about that user. If you or anyone you know plays CS:GO please, give it a try!</p>
-            <img src="#" className="csgoSS"></img>
           </div>
         </ul>
       </li >
